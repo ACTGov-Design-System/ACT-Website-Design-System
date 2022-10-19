eebd4ef24ef292c233747acad20b26ba32c88009
Tom Milburn <tom@milbs.net>
2022-10-19 21:53:37 +1100

    wednesday save

--- src/4-components/F1-Footers/F1-1-Footer-AoC/component.js
+++ src/4-components/F1-Footers/F1-1-Footer-AoC/component.js
@@ -7,12 +7,12 @@ export const createF11 = ({}) => {
 
 				<div class="act-col act-col-sm-12">
 					<div class="act-box">
-						<div class="act-aoc">
-							<div class="act-aoc__title">
+						<div class="act-footer-aoc">
+							<div class="act-footer-aoc__title">
 							  <i>&nbsp;</i>
 						    <h4 class="act-h4-white">Acknowledgement of Country</h4>
 					    </div>
-							<div class="act-aoc__body">
+							<div class="act-footer-aoc__body">
 								<p class="act-p-large act-p-white">We acknowledge the Traditional Custodians of the ACT, the Ngunnawal people. We acknowledge and respect their continuing culture and the contribution they make to the life of this city and this region.</p>
 							</div>
 					  </div>
--- src/5-patterns/T1-Wayfinding/T1-1-TopBar/component.js
+++ src/5-patterns/T1-Wayfinding/T1-1-TopBar/component.js
@@ -1,12 +1,18 @@
-import { createB41 } from '../../../4-components/B4-Buttons/B4-1-Primary-Button/component';
 import { createL22 } from '../../../4-components/L2-Logos/L2-2-Cobranded-Logo/component';
-import { createF11 } from '../../../4-components/F1-Footers/F1-1-Footer-AoC/component';
+import { createA21 } from '../../../4-components/A2-Alerts/A2-1-Alert-Global/component';
 import { createM31 } from '../../../4-components/M3-Masthead/M3-1-Masthead/component';
 import { createW16 } from '../../../4-components/W1-Wayfinding/W1-6-NavBar/component';
 
 
 export const createT11 = ({}) => {
 
+	const alertHTML = createA21({
+		type: "success",
+		alert: "<span>Free registration</span> for zero emission vehicles for two years",
+		fontawesome: "fa-leaf",
+		icon: "icon-left",
+		dismissible: true,
+	});
 	const mastheadHTML = createM31({
 		title: "An ACT Government Website",
 		buttonText: "Log In"
@@ -14,9 +20,8 @@ export const createT11 = ({}) => {
 	const navbarHTML = createW16({
 		title: "An ACT Government Website1",
 	});
-	const aocHTML = createF11({});
 
-	const topbarHTML = mastheadHTML + navbarHTML + aocHTML;
+	const topbarHTML = alertHTML + mastheadHTML + navbarHTML;
 
 	const topbar = document.createElement('div');
 	topbar.className = 'sb-act-topbar';
